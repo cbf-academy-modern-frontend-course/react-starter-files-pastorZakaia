7c7890e660667e63648eb581a2b672690f1f5ad2
pastorZakaia <94982635+pastorZakaia@users.noreply.github.com>
2022-11-01 19:49:25 +0000

    Zakiya's react page

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+const name = 'tom';
 
-function Profile(name){
+function Profile(name){ 
+  if (name) {
     return <main id="content" role="main" className="base">
             <h1>{name}'s React Page</h1>
               <p>Example of react JSX in action</p>
             </main>
+  } else {
+    return <main id="content" role="main" className="base">
+    <h1>Just a React Page</h1>
+      <p>Nothing to see here!</p>
+    </main>
+    
+  }
+    
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+root.render(Profile("Zakiya"));
 
