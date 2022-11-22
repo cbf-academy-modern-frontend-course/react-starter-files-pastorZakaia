35c4617242c8074dd1e342491c49e921fda927e9
pastorZakaia <94982635+pastorZakaia@users.noreply.github.com>
2022-11-22 20:00:31 +0000

    Task 2 week 7

--- routes-and-forms/src/components/PetForm.js
+++ routes-and-forms/src/components/PetForm.js
@@ -3,6 +3,13 @@ import React, { useState } from 'react';
 const PetForm = () => {
   const [name, setName] = useState('');
   const [,setSubmitted] = useState(false);
+  const [age, setAge] = useState('');
+  const [colours, setColours] = useState([]);
+
+  
+
+
+  
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -14,9 +21,14 @@ const PetForm = () => {
       <h2>Register Your Pet</h2>
       <h3>Please fill in your pet's details below</h3>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
+        
+        
           <label>Pet's Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
-        </div>
+          <label>Pet's Age:<input type="text" value={age} onChange={(e) => { setAge(e.target.value); setSubmitted(false) }} /></label>
+          <label>Pet's colours:<input type="text" value={colours} onChange={(e) => { setColours(e.target.value); setSubmitted(false) }} /></label>
+
+       
+      
         <input type="submit" value="Submit" id="submit-button"/>
       </form>
       <div id="results">
