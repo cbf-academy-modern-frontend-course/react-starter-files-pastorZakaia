15c111521c78550b56d16cbc8e5967d4ed54a66e
pastorZakaia <94982635+pastorZakaia@users.noreply.github.com>
2022-11-23 18:32:50 +0000

    part 7 and 8 tasks

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -6,21 +6,31 @@ import bookList from './components/BookList';
 // import {Route, Routes} from 'react-router-dom';
 
 import Search from './components/Search'
+import keyword from './components/Search'
 
 
 
 
 function App() {
   const [books, setbooks] = useState(data);
+  async function findBooks(value) {
+    const URL = `https://www.googleapis.com/books/v1/volumes?q=$%7Bvalue%7D&filter=paid-ebooks&print-type=books&projection=lite`;
+  
+    const results =await fetch(url).then(res => res.json());
+    if (!results.error) {
+      setbooks(results.items);
+    }
+  }
   // const books = data; line 9 and 10 do the same thing
   return (
   <div>
      <h2>Welcome to my e-bookshop</h2>
-    <Search/>
+    <Search findBooks ={findBooks} keyword={keyword} setKeyword={setKeyword}/>
     
     <bookList book={bookList}/>
     {books.map((book) => <Book key={book.id} book={book} />)}
  
 </div> ) 
 }
+
 export default App;
