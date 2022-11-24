5e1df35d5e4652b1552cb9bb381bb63f5515229b
pastorZakaia <94982635+pastorZakaia@users.noreply.github.com>
2022-11-24 19:18:18 +0000

    task 1 week 8

--- bookcase-app/src/components/Header.js
+++ bookcase-app/src/components/Header.js
@@ -8,11 +8,11 @@ import Navpages from
 
 
 function header() {
-    return
-    <>
+    return (<>
      <h1> My Bookcase</h1>
     <Link to = '/'>Home</Link>
     <Link to ="/bookcase" className='bookLink'>Bookcase</Link>
-    </>
+    </>)
+    
 }
 export default header;
